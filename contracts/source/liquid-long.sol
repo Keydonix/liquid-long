94bf104ef99f4b4806728dd77390c380820c7dd3
Scott Bigelow <epheph@gmail.com>
2018-06-12 22:25:35 -0700

    safe math, change pay_remaining logic

--- contracts/source/liquid-long.sol
+++ contracts/source/liquid-long.sol
@@ -3,6 +3,56 @@ pragma experimental ABIEncoderV2;
 pragma experimental "v0.5.0";
 
 /**
+* @title SafeMath
+* @dev Math operations with safety checks that throw on error
+* https://github.com/OpenZeppelin/openzeppelin-solidity/blob/56515380452baad9fcd32c5d4502002af0183ce9/contracts/math/SafeMath.sol
+*/
+library SafeMath {
+
+	/**
+	* @dev Multiplies two numbers, throws on overflow.
+	*/
+	function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+		// Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+		// benefit is lost if 'b' is also tested.
+		// See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+		if (a == 0) {
+			return 0;
+		}
+		c = a * b;
+		assert(c / a == b);
+		return c;
+	}
+
+	/**
+	* @dev Integer division of two numbers, truncating the quotient.
+	*/
+	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+		// assert(b > 0); // Solidity automatically throws when dividing by 0
+		// uint256 c = a / b;
+		// assert(a == b * c + a % b); // There is no case in which this doesn't hold
+		return a / b;
+	}
+
+	/**
+	* @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+	*/
+	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b <= a);
+		return a - b;
+	}
+
+	/**
+	* @dev Adds two numbers, throws on overflow.
+	*/
+	function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+		c = a + b;
+		assert(c >= a);
+		return c;
+	}
+}
+
+/**
  * @title ERC20Basic
  * @dev Simpler version of ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/179
@@ -221,6 +271,8 @@ contract Maker {
 }
 
 contract LiquidLong is Ownable, Claimable, Pausable {
+	using SafeMath for uint256;
+
 	Oasis public oasis;
 	Maker public maker;
 	Dai private dai;
@@ -269,11 +321,11 @@ contract LiquidLong is Ownable, Claimable, Pausable {
 		uint256 _offerId = oasis.getBestOffer(_buyGem, _payGem);
 		while (_offerId != 0) {
 			(uint256 _offerPayAmount, , uint256 _offerBuyAmount,) = oasis.getOffer(_offerId);
-			if (_offerPayAmount * _payAmount < _offerBuyAmount * _buyAmount) {
+			if (_offerPayAmount.mul(_payAmount) < _offerBuyAmount.mul(_buyAmount)) {
 				break;
 			}
-			_fillPayAmount += _offerBuyAmount;
-			_fillBuyAmount += _offerPayAmount;
+			_fillPayAmount = _fillPayAmount.add(_offerBuyAmount);
+			_fillBuyAmount = _fillBuyAmount.add(_offerPayAmount);
 			_offerId = oasis.getWorseOffer(_offerId);
 		}
 		return (_fillPayAmount, _fillBuyAmount);
@@ -283,17 +335,18 @@ contract LiquidLong is Ownable, Claimable, Pausable {
 		uint256 _offerId = oasis.getBestOffer(_buyGem, _payGem);
 		while (_offerId != 0 && _buyAmount > _fillBuyAmount) {
 			(uint256 _offerPayAmount, , uint256 _offerBuyAmount,) = oasis.getOffer(_offerId);
-			if (_fillBuyAmount + _offerPayAmount > _buyAmount) {
+			if (_fillBuyAmount.add(_offerPayAmount) > _buyAmount) {
 				uint256 _buyRemaining = _buyAmount - _fillBuyAmount;
 				// + 1 required here to in case _offerBuyAmount / _offerPayAmount is rounded down.
 				// You get to choose between buying less than or more than
-				uint256 _payRemaining = (_buyRemaining + 1) * _offerBuyAmount / _offerPayAmount;
-				_fillPayAmount += _payRemaining;
-				_fillBuyAmount += _buyRemaining;
+				// TODO: safe math after verying this logic is correct
+				uint256 _payRemaining = (_buyRemaining * _offerBuyAmount / _offerPayAmount) + 1;
+				_fillPayAmount = _fillPayAmount.add(_payRemaining);
+				_fillBuyAmount = _fillBuyAmount.add(_buyRemaining);
 				break;
 			}
-			_fillPayAmount += _offerBuyAmount;
-			_fillBuyAmount += _offerPayAmount;
+			_fillPayAmount = _fillPayAmount.add(_offerBuyAmount);
+			_fillBuyAmount = _fillBuyAmount.add(_offerPayAmount);
 			_offerId = oasis.getWorseOffer(_offerId);
 		}
 		return (_fillPayAmount, _fillBuyAmount);
