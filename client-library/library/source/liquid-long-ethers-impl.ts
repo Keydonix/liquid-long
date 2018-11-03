5fe6ff43b00f6b51ec5dd42859c4dee7dcdf2b77
Alex Novikov on mac-mini <alex@asnovikov.ru>
2018-11-03 20:01:44 -0300

    imported from root instead of submodules to use single entrance point which is cross-enrironment unlike submodules; fixed Unterminated statement Warnings;

--- client-library/library/source/liquid-long-ethers-impl.ts
+++ client-library/library/source/liquid-long-ethers-impl.ts
@@ -1,6 +1,15 @@
-import { Dependencies, AbiFunction, AbiParameter, Transaction } from './generated/liquid-long'
-import { keccak256, toUtf8Bytes, BigNumber, AbiCoder } from 'ethers/utils'
-import { TransactionResponse, TransactionRequest } from 'ethers/providers';
+import { Dependencies, AbiFunction, AbiParameter, Transaction } from './generated/liquid-long';
+
+import { ethers } from 'ethers';
+// import { keccak256, toUtf8Bytes, BigNumber, AbiCoder } from 'ethers/utils';
+type BigNumber = ethers.utils.BigNumber;
+const keccak256 = ethers.utils.keccak256;
+const toUtf8Bytes = ethers.utils.toUtf8Bytes;
+const AbiCoder = ethers.utils.AbiCoder;
+// import { TransactionResponse, TransactionRequest } from 'ethers/providers';
+type TransactionResponse = ethers.providers.TransactionResponse;
+type TransactionRequest = ethers.providers.TransactionRequest;
+
 
 export interface Provider {
 	listAccounts(): Promise<Array<string>>
@@ -12,17 +21,17 @@ export interface Signer {
 }
 
 export class LiquidLongDependenciesEthers implements Dependencies<BigNumber> {
-	private readonly provider: Provider
-	private readonly signer: Signer
+	private readonly provider: Provider;
+	private readonly signer: Signer;
 	public constructor(provider: Provider, signer: Signer) {
-		this.provider = provider
+		this.provider = provider;
 		this.signer = signer
 	}
 
-	keccak256 = (utf8String: string) => keccak256(toUtf8Bytes(utf8String))
-	encodeParams = (abiFunction: AbiFunction, parameters: Array<any>) => new AbiCoder().encode(abiFunction.inputs, parameters).substr(2)
-	decodeParams = (abiParameters: Array<AbiParameter>, encoded: string) => new AbiCoder().decode(abiParameters, encoded)
-	getDefaultAddress = async () => (await this.provider.listAccounts())[0]
-	call = async (transaction: Transaction<BigNumber>) => await this.provider.call(transaction)
+	keccak256 = (utf8String: string) => keccak256(toUtf8Bytes(utf8String));
+	encodeParams = (abiFunction: AbiFunction, parameters: Array<any>) => new AbiCoder().encode(abiFunction.inputs, parameters).substr(2);
+	decodeParams = (abiParameters: Array<AbiParameter>, encoded: string) => new AbiCoder().decode(abiParameters, encoded);
+	getDefaultAddress = async () => (await this.provider.listAccounts())[0];
+	call = async (transaction: Transaction<BigNumber>) => await this.provider.call(transaction);
 	submitTransaction = async (transaction: Transaction<BigNumber>) => ({ status: (await (await this.signer.sendTransaction(transaction)).wait()).status! })
 }
--- client-library/library/source/liquid-long.ts
+++ client-library/library/source/liquid-long.ts
@@ -2,18 +2,23 @@ import { LiquidLong as LiquidLongContract } from './generated/liquid-long'
 import { LiquidLongDependenciesEthers, Provider, Signer } from './liquid-long-ethers-impl'
 import { Scheduler } from './scheduler'
 import { PolledValue } from './polled-value'
-import { BigNumber, bigNumberify } from 'ethers/utils'
+
+import { ethers } from 'ethers';
+// import { BigNumber, bigNumberify } from 'ethers/utils';
+type BigNumber = ethers.utils.BigNumber;
+const bigNumberify = ethers.utils.bigNumberify;
+
 
 export class LiquidLong {
-	private readonly contract: LiquidLongContract<BigNumber>
-	private readonly ethPriceInUsd: PolledValue<number>
-	private readonly providerFeeRate: PolledValue<number>
-	public readonly awaitReady: Promise<void>
+	private readonly contract: LiquidLongContract<BigNumber>;
+	private readonly ethPriceInUsd: PolledValue<number>;
+	private readonly providerFeeRate: PolledValue<number>;
+	public readonly awaitReady: Promise<void>;
 
 	public constructor(scheduler: Scheduler, provider: Provider, signer: Signer, liquidLongAddress: string, defaultEthPriceInUsd: number, defaultProviderFeeRate: number, ethPricePollingFrequency: number = 10000, providerFeePollingFrequency: number = 10000) {
-		this.contract = new LiquidLongContract(new LiquidLongDependenciesEthers(provider, signer), liquidLongAddress)
-		this.ethPriceInUsd = new PolledValue(scheduler, this.fetchEthPriceInUsd, ethPricePollingFrequency, defaultEthPriceInUsd)
-		this.providerFeeRate = new PolledValue(scheduler, this.fetchProviderFeeRate, providerFeePollingFrequency, defaultProviderFeeRate)
+		this.contract = new LiquidLongContract(new LiquidLongDependenciesEthers(provider, signer), liquidLongAddress);
+		this.ethPriceInUsd = new PolledValue(scheduler, this.fetchEthPriceInUsd, ethPricePollingFrequency, defaultEthPriceInUsd);
+		this.providerFeeRate = new PolledValue(scheduler, this.fetchProviderFeeRate, providerFeePollingFrequency, defaultProviderFeeRate);
 		this.awaitReady = Promise.all([this.ethPriceInUsd.latest, this.providerFeeRate.latest]).then(() => {})
 	}
 
@@ -22,102 +27,102 @@ export class LiquidLong {
 			this.ethPriceInUsd.shutdown(),
 			this.providerFeeRate.shutdown(),
 		])
-	}
+	};
 
 	public registerForEthPriceUpdated = (listener: (newEthPriceInUsd: number) => void): void => {
 		this.ethPriceInUsd.registerListener(listener)
-	}
+	};
 
 	public getEthPriceInUsd = async (): Promise<number> => {
 		return await this.ethPriceInUsd.cached
-	}
+	};
 
 	public getLiquidationPriceInUsd = async (leverageMultiplier: number): Promise<number> => {
-		const ethPrice = await this.ethPriceInUsd.cached
-		const liquidationAsPercentOfPrice =  1.5 - 1.5 / leverageMultiplier
+		const ethPrice = await this.ethPriceInUsd.cached;
+		const liquidationAsPercentOfPrice =  1.5 - 1.5 / leverageMultiplier;
 		return ethPrice * liquidationAsPercentOfPrice
-	}
+	};
 
 	public getFuturePriceInUsdForPercentChange = async (percentChangeFromCurrent: number, leverageMultiplier: number): Promise<number> => {
-		const ethPrice = await this.ethPriceInUsd.cached
+		const ethPrice = await this.ethPriceInUsd.cached;
 		return ethPrice * (1 + percentChangeFromCurrent / leverageMultiplier)
-	}
+	};
 
 	public getPercentageChangeForFuturePrice = async (futurePriceInUsd: number, leverageMultiplier: number): Promise<number> => {
-		const ethPrice = await this.ethPriceInUsd.cached
+		const ethPrice = await this.ethPriceInUsd.cached;
 		return leverageMultiplier * (futurePriceInUsd / ethPrice - 1)
-	}
+	};
 
 	public getPositionValueInUsdAtFuturePrice = async (futurePriceInUsd: number, leverageMultiplier: number, leverageSizeInEth: number): Promise<number> => {
-		const percentageChange = await this.getPercentageChangeForFuturePrice(futurePriceInUsd, leverageMultiplier)
-		const ethAtPrice = leverageSizeInEth + leverageSizeInEth * percentageChange
+		const percentageChange = await this.getPercentageChangeForFuturePrice(futurePriceInUsd, leverageMultiplier);
+		const ethAtPrice = leverageSizeInEth + leverageSizeInEth * percentageChange;
 		return ethAtPrice * await this.ethPriceInUsd.cached
-	}
+	};
 
 	public getChangeInPositionValueInUsdAtFuturePrice = async (futurePriceInUsd: number, leverageMultiplier: number, leverageSizeInEth: number): Promise<number> => {
-		const currentPositionValueInUsd = await this.getPositionValueInUsdAtFuturePrice(await this.ethPriceInUsd.cached, leverageMultiplier, leverageSizeInEth)
-		const futurePositionValueInUsd =  await this.getPositionValueInUsdAtFuturePrice(futurePriceInUsd, leverageMultiplier, leverageSizeInEth)
+		const currentPositionValueInUsd = await this.getPositionValueInUsdAtFuturePrice(await this.ethPriceInUsd.cached, leverageMultiplier, leverageSizeInEth);
+		const futurePositionValueInUsd =  await this.getPositionValueInUsdAtFuturePrice(futurePriceInUsd, leverageMultiplier, leverageSizeInEth);
 		return futurePositionValueInUsd - currentPositionValueInUsd
-	}
+	};
 
 	public getFeeInEth = async (leverageMultiplier: number, leverageSizeInEth: number): Promise<number> => {
-		const providerFeeRate = await this.providerFeeRate.cached
-		const loanInEth = this.getLoanSizeInEth(leverageMultiplier, leverageSizeInEth)
-		const feeInEth = loanInEth * providerFeeRate
+		const providerFeeRate = await this.providerFeeRate.cached;
+		const loanInEth = this.getLoanSizeInEth(leverageMultiplier, leverageSizeInEth);
+		const feeInEth = loanInEth * providerFeeRate;
 		return feeInEth
-	}
+	};
 
 	// TODO verify this math with a run through of a liquidation
 	public getLiquidationPenaltyPercent = (leverageMultiplier: number): number => {
-		const liquidationAsPercentOfPrice = 1.5 - 1.5 / leverageMultiplier
+		const liquidationAsPercentOfPrice = 1.5 - 1.5 / leverageMultiplier;
 		return leverageMultiplier * (liquidationAsPercentOfPrice * (1 - 0.13 / leverageMultiplier) - 1)
-	}
+	};
 
 	public getEstimatedCostsInEth = async (leverageMultiplier: number, leverageSizeInEth: number): Promise<{low: number, high: number}> => {
-		const daiPerEth = this.ethPriceInUsd.cached
-		const loanSizeInEth = this.getLoanSizeInEth(leverageMultiplier, leverageSizeInEth)
-		const daiToSell = loanSizeInEth * daiPerEth
-		const attodaiToSell = bigNumberify(Math.floor(daiToSell * 1e9)).mul(1e9)
-		const result = await this.contract.estimateDaiSaleProceeds_(attodaiToSell)
-		const daiSaleProceedsInEth = result._wethBought.div(1e9).toNumber() / 1e9
-		const estimatedCostInEth = loanSizeInEth - daiSaleProceedsInEth
-		const low = estimatedCostInEth
-		const high = (low > 0) ? low * 2 : 0
+		const daiPerEth = this.ethPriceInUsd.cached;
+		const loanSizeInEth = this.getLoanSizeInEth(leverageMultiplier, leverageSizeInEth);
+		const daiToSell = loanSizeInEth * daiPerEth;
+		const attodaiToSell = bigNumberify(Math.floor(daiToSell * 1e9)).mul(1e9);
+		const result = await this.contract.estimateDaiSaleProceeds_(attodaiToSell);
+		const daiSaleProceedsInEth = result._wethBought.div(1e9).toNumber() / 1e9;
+		const estimatedCostInEth = loanSizeInEth - daiSaleProceedsInEth;
+		const low = estimatedCostInEth;
+		const high = (low > 0) ? low * 2 : 0;
 		return { low, high }
-	}
+	};
 
 	public openPosition = async (leverageMultiplier: number, leverageSizeInEth: number, costLimitInEth: number, feeLimitInEth: number): Promise<void> => {
-		const leverageMultiplierInPercents = bigNumberify(Math.round(leverageMultiplier * 100))
-		const leverageSizeInAttoeth = bigNumberify(Math.floor(leverageSizeInEth * 1e9)).mul(1e9)
-		const allowedCostInAttoeth = bigNumberify(Math.floor(costLimitInEth * 1e9)).mul(1e9)
-		const allowedFeeInAttoeth = bigNumberify(Math.floor(feeLimitInEth * 1e9)).mul(1e9)
-		const affiliateFeeInAttoeth = bigNumberify(0)
-		const affiliateAddress = '0x0000000000000000000000000000000000000000'
-		const totalAttoeth = leverageSizeInAttoeth.add(allowedCostInAttoeth).add(allowedFeeInAttoeth).add(affiliateFeeInAttoeth)
+		const leverageMultiplierInPercents = bigNumberify(Math.round(leverageMultiplier * 100));
+		const leverageSizeInAttoeth = bigNumberify(Math.floor(leverageSizeInEth * 1e9)).mul(1e9);
+		const allowedCostInAttoeth = bigNumberify(Math.floor(costLimitInEth * 1e9)).mul(1e9);
+		const allowedFeeInAttoeth = bigNumberify(Math.floor(feeLimitInEth * 1e9)).mul(1e9);
+		const affiliateFeeInAttoeth = bigNumberify(0);
+		const affiliateAddress = '0x0000000000000000000000000000000000000000';
+		const totalAttoeth = leverageSizeInAttoeth.add(allowedCostInAttoeth).add(allowedFeeInAttoeth).add(affiliateFeeInAttoeth);
 		await this.contract.openCdp(leverageMultiplierInPercents, leverageSizeInAttoeth, allowedFeeInAttoeth, affiliateFeeInAttoeth, affiliateAddress, { attachedEth: totalAttoeth })
-	}
+	};
 
 	public adminDepositEth = async (amount: number): Promise<void> => {
 		await this.contract.wethDeposit({ attachedEth: bigNumberify(amount).mul(1e18.toString()) })
-	}
+	};
 
 	public adminWithdrawEth = async (amount: number): Promise<void> => {
 		await this.contract.wethWithdraw(bigNumberify(amount).mul(1e18.toString()))
-	}
+	};
 
 	private fetchEthPriceInUsd = async (): Promise<number> => {
-		const attousd = await this.contract.ethPriceInUsd_()
+		const attousd = await this.contract.ethPriceInUsd_();
 		return attousd.div(1e9).toNumber() / 1e9
-	}
+	};
 
 	private fetchProviderFeeRate = async (): Promise<number> => {
-		const providerFeeAttoethPerEth = await this.contract.providerFeePerEth_()
+		const providerFeeAttoethPerEth = await this.contract.providerFeePerEth_();
 		return providerFeeAttoethPerEth.div(1e9).toNumber() / 1e9
-	}
+	};
 
 	private getLoanSizeInEth = (leverageMultiplier: number, leverageSizeInEth: number): number => {
-		const ethLockedInCdp = leverageSizeInEth * leverageMultiplier
-		const loanInEth = ethLockedInCdp - leverageSizeInEth
+		const ethLockedInCdp = leverageSizeInEth * leverageMultiplier;
+		const loanInEth = ethLockedInCdp - leverageSizeInEth;
 		return loanInEth
 	}
 }
