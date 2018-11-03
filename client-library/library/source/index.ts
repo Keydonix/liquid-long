206e5dfb0882c5275397db45c1c26ccf147110f4
Alex Novikov on mac-mini <alex@asnovikov.ru>
2018-11-03 19:50:59 -0300

    removed export from ethers - if somebody needs it she will import them from ethers.js itself;

--- client-library/library/source/index.ts
+++ client-library/library/source/index.ts
@@ -2,5 +2,3 @@ export { LiquidLong } from './liquid-long'
 export { Scheduler, TimeoutScheduler } from './scheduler'
 export { PolledValue } from './polled-value'
 export { Provider, Signer } from './liquid-long-ethers-impl'
-export { BigNumber, bigNumberify } from 'ethers/utils'
-export { JsonRpcProvider, TransactionResponse, TransactionRequest, TransactionReceipt } from 'ethers/providers';
