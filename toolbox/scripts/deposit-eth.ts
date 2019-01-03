d953e3d3ee1986a3c15f87ec0d30d32abcf58231
Micah Zoltu <micah@zoltu.net>
2019-01-03 16:47:50 +0800

    Toolbox fixup.
    Haven't used the toolbox in a while, so some of the scripts were non-functional.  Just fixes them to be functional or removes functionality that can't be fixed.

--- toolbox/scripts/deposit-eth.ts
+++ toolbox/scripts/deposit-eth.ts
@@ -10,7 +10,7 @@ async function doStuff() {
 	// gather/validate inputs
 	const jsonRpcAddress = getEnv('ETHEREUM_HTTP', 'http://localhost:1235')
 	const gasPrice = parseInt(getEnv('ETHEREUM_GAS_PRICE_IN_NANOETH', '1'), 10)
-	const liquidLongAddress = getEnv('ETHEREUM_LIQUID_LONG_ADDRESS', '0x80F8DAA435A9AB4B1802BA56FE7E0ABD0F8AB3D3')
+	const liquidLongAddress = getEnv('ETHEREUM_LIQUID_LONG_ADDRESS', 'f3bcabd8fae29f75be271ebe2499edb4c7c139b7')
 	const privateKey = PrivateKey.fromHexString(getEnv('ETHEREUM_PRIVATE_KEY', 'fae42052f82bed612a724fec3632f325f377120592c75bb78adfcceae6470c5a'))
 
 	const provider = new JsonRpcProvider(jsonRpcAddress, 4173)
@@ -18,8 +18,7 @@ async function doStuff() {
 
 	const dependencies = new LiquidLongDependenciesEthers(provider, wallet, gasPrice)
 	const liquidLong = new LiquidLong(dependencies, liquidLongAddress)
-	// FIXME: once new images are published, we can get wethAddress from `await liquidLong.weth_()`
-	const weth = new Weth(dependencies, '0xfcaf25bf38e7c86612a25ff18cb8e09ab07c9885')
+	const weth = new Weth(dependencies, await liquidLong.weth_())
 	console.log(`Depositing 100 ETH into Liquid Long.`)
 	await liquidLong.wethDeposit({ attachedEth: bigNumberify(100).mul(1e18.toString()) })
 	console.log(`Confirming new balance.`)
