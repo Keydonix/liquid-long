039d3a0c82c1d907a1832361cfd7038f54139077
Micah Zoltu <micah@zoltu.net>
2019-01-16 20:34:43 +0800

    Updates SafeMath library from OpenZeppelin.
    For some reason the version we were using used `assert` instead of `require` so it was needlessly burning gas.
    
    The lockfile changes are due to a new version of NPM.  The transitive dependencies now are weak links, but the actual downloaded dependencies are still fixed.

--- contracts/source/liquid-long.sol
+++ contracts/source/liquid-long.sol
@@ -3,54 +3,118 @@ pragma experimental ABIEncoderV2;
 pragma experimental "v0.5.0";
 
 /**
-* @title SafeMath
-* @dev Math operations with safety checks that throw on error
-* https://github.com/OpenZeppelin/openzeppelin-solidity/blob/56515380452baad9fcd32c5d4502002af0183ce9/contracts/math/SafeMath.sol
-*/
+ * @title SafeMath
+ * @dev Math operations with safety checks that revert on error
+ * https://github.com/OpenZeppelin/openzeppelin-solidity/blob/d17ae0b806b2f8e69b291284bbf30321640609e3/contracts/math/SafeMath.sol
+ */
 library SafeMath {
+	int256 constant private INT256_MIN = -2**255;
 
 	/**
-	* @dev Multiplies two numbers, throws on overflow.
+	* @dev Multiplies two unsigned integers, reverts on overflow.
 	*/
-	function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-		// Gas optimization: this is cheaper than asserting 'a' not being zero, but the
-		// benefit is lost if 'b' is also tested.
+	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+		// Gas optimization: this is cheaper than requiring 'a' not being zero, but the benefit is lost if 'b' is also tested.
 		// See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
 		if (a == 0) {
 			return 0;
 		}
-		c = a * b;
-		assert(c / a == b);
+
+		uint256 c = a * b;
+		require(c / a == b);
+
 		return c;
 	}
 
 	/**
-	* @dev Integer division of two numbers, truncating the quotient.
+	* @dev Multiplies two signed integers, reverts on overflow.
+	*/
+	function mul(int256 a, int256 b) internal pure returns (int256) {
+		// Gas optimization: this is cheaper than requiring 'a' not being zero, but the benefit is lost if 'b' is also tested.
+		// See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+		if (a == 0) {
+			return 0;
+		}
+
+		require(!(a == -1 && b == INT256_MIN)); // This is the only case of overflow not detected by the check below
+
+		int256 c = a * b;
+		require(c / a == b);
+
+		return c;
+	}
+
+	/**
+	* @dev Integer division of two unsigned integers truncating the quotient, reverts on division by zero.
 	*/
 	function div(uint256 a, uint256 b) internal pure returns (uint256) {
-		// assert(b > 0); // Solidity automatically throws when dividing by 0
-		// uint256 c = a / b;
+		// Solidity only automatically asserts when dividing by 0
+		require(b > 0);
+		uint256 c = a / b;
 		// assert(a == b * c + a % b); // There is no case in which this doesn't hold
-		return a / b;
-	}
 
-	/**
-	* @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-	*/
-	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-		assert(b <= a);
-		return a - b;
-	}
-
-	/**
-	* @dev Adds two numbers, throws on overflow.
-	*/
-	function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-		c = a + b;
-		assert(c >= a);
 		return c;
 	}
 
+	/**
+	* @dev Integer division of two signed integers truncating the quotient, reverts on division by zero.
+	*/
+	function div(int256 a, int256 b) internal pure returns (int256) {
+		require(b != 0); // Solidity only automatically asserts when dividing by 0
+		require(!(b == -1 && a == INT256_MIN)); // This is the only case of overflow
+
+		int256 c = a / b;
+
+		return c;
+	}
+
+	/**
+	* @dev Subtracts two unsigned integers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+	*/
+	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		require(b <= a);
+		uint256 c = a - b;
+
+		return c;
+	}
+
+	/**
+	* @dev Subtracts two signed integers, reverts on overflow.
+	*/
+	function sub(int256 a, int256 b) internal pure returns (int256) {
+		int256 c = a - b;
+		require((b >= 0 && c <= a) || (b < 0 && c > a));
+
+		return c;
+	}
+
+	/**
+	* @dev Adds two unsigned integers, reverts on overflow.
+	*/
+	function add(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a + b;
+		require(c >= a);
+
+		return c;
+	}
+
+	/**
+	* @dev Adds two signed integers, reverts on overflow.
+	*/
+	function add(int256 a, int256 b) internal pure returns (int256) {
+		int256 c = a + b;
+		require((b >= 0 && c >= a) || (b < 0 && c < a));
+
+		return c;
+	}
+
+	/**
+	* @dev Divides two unsigned integers and returns the remainder (unsigned integer modulo), reverts when dividing by zero.
+	*/
+	function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+		require(b != 0);
+		return a % b;
+	}
 }
 
 /**
